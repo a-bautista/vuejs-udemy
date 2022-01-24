d98e5aa82ec0b5ea3d5449fcd0a1b58113b6ef3e
Alejandro Bautista R <alex.bautista.ramos.90@gmail.com>
2022-01-23 16:48:53 -0800

    added the chapter for using vue with typescript

---
+++ 14-VueAndTypescript/vue-typescript/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <p> {{ jobs[0].location }} </p>
+    <!-- <button @click="changeName('Emily')">Change the name</button>
+    <button @click="changeAge(25)">Change the age</button> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "Farmer", location: "Lon Lon Ranch", salary: 3000, id: '1' },
+      { title: "Prison Guard", location: "Gerudo Valley", salary: 40000, id: '2'},
+      { title: "Fisherman", location: "Lake Hylia", salary: 20000, id: '3'},
+    ])
+    return { jobs }
+  }
+
+  //   data() {
+  //     return {
+  //       name: 'Alejandro',
+  //       age: 31 as number | string // type assertion
+  //   }
+  // },
+  // methods: {
+  //   changeName(name: string) {
+  //       this.name = name; 
+  //     },
+  //   changeAge(age: number | string){
+  //     this.age = age;
+  //   }
+  // }
+});
+</script>
+
+<style>
+
+</style>
